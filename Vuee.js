a368c2bfc7ca1b1b3c4895cfb50178c0c97e0d8e
singcl <24661881@qq.com>
2018-06-13 17:20:03 +0800

    :tada: vue 响应式特性探索

---
+++ Vuee.js
@@ -0,0 +1,56 @@
+// 极简的类Vue 数据响应式框架
+class Vuee {
+    constructor(options) {
+        this.options = options
+        this.observe(options.data)
+        this.compile(document.querySelector(options.el))
+    }
+
+    observe(data) {
+        Object.keys(data).forEach((key) => {
+            let ob = new Observer()
+            let val = data[key]
+            Object.defineProperty(data, key, {
+                get() {
+                    Observer.target && ob.addSubNode(Observer.target)
+                    return val
+                },
+                
+                set(newVal) {
+                    ob.update(newVal)
+                    val = newVal
+                }
+            })
+        })
+    }
+
+    compile(node) {
+        [].forEach.call(node.childNodes, (child) => {
+            if (!child.firstElementNode && /\{\{(.*)\}\}/.test(child.innerHTML)) {
+                let key = RegExp.$1.trim()
+                child.innerHTML = child.innerHTML.replace(new RegExp('{{\\s*' + key + '\\s*}}', 'gm'), this.options.data[key])
+                Observer.target = child
+                this.options.data[key]
+                Observer.target = null
+            } else if (child.firstElementNode) {
+                this.compile(child)
+            }
+        })
+    }
+}
+
+class Observer {
+    constructor() {
+        this.subNode = []
+    }
+
+    addSubNode(node) {
+        this.subNode.push(node)
+    }
+
+    update(newVal) {
+        this.subNode.forEach((node) => {
+            node.innerHTML = newVal
+        })
+    }
+}
